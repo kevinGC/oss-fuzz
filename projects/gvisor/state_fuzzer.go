1917c2f18274f2ff396bf9aea20f1ac2d3574ff8
Kevin Krakauer <krakauer@google.com>
2022-06-08 18:49:35 -0700

    gvisor: ignore OOM panics
    OSS-Fuzz is fuzzing deserialization code, where objects are prepended
    with headers that includes the object lengths. When one of those lengths
    is too large, malloc fails and causes a panic. This is what we want to
    happen.
    
    This change works around this by having the test launch a subprocess.
    The subprocess runs the actual fuzzing test, and the parent passes along
    the subprocess return value unless it sees an OOM.
    
    Fixes #7826.

--- projects/gvisor/state_fuzzer.go
+++ projects/gvisor/state_fuzzer.go
@@ -18,13 +18,72 @@ package fuzzing
 import (
 	"bytes"
 	"context"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
 	"gvisor.dev/gvisor/pkg/buffer"
 	"gvisor.dev/gvisor/pkg/state"
 )
 
 func FuzzStateLoad(data []byte) int {
-	ctx := context.Background()
-	var toLoad *buffer.View
-	_, _ = state.Load(ctx, bytes.NewReader(data), toLoad)
+	const filename = "data"
+
+	isChild := flag.Bool("child", false, "whether this process is the test child process")
+	flag.Parse()
+
+	if *isChild {
+		dataFile, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer dataFile.Close()
+
+		dataBytes, err := io.ReadAll(dataFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ctx := context.Background()
+		var toLoad *buffer.View
+		_, _ = state.Load(ctx, bytes.NewReader(dataBytes), toLoad)
+		return 1
+	}
+
+	dataFile, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(filename)
+
+	if _, err := dataFile.Write(data); err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := exec.Command("/proc/self/exe", "-child")
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	stderrOutput, err := io.ReadAll(stderr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		// It is intended behavior to panic if the input causes an OOM.
+		if !strings.Contains(string(stderrOutput), "runtime: out of memory: cannot allocate") {
+			log.Fatal(err)
+		}
+	}
+
 	return 1
 }
